183cf170b360516a94bace217ec390591d834835
titiksha <titiksha.iyer@gmail.com>
2023-12-06 22:26:54 +0530

    7 dec wind up

--- app.js
+++ app.js
@@ -3,12 +3,32 @@
 //     bodycolor="black";
     
 // }
-    function changetheme(){
-        if(document.body.style.backgroundColor==="black"){
-            document.body.style.backgroundColor="white";
-        }
-        else{
-            document.body.style.backgroundColor="black";
-        }
+    // function changetheme(){
+    //     if(document.body.style.backgroundColor==="black"){
+    //         document.body.style.backgroundColor="white";
+    //     }
+    //     else{
+    //         document.body.style.backgroundColor="black";
+    //     }
         
+    // }
+
+
+    function changetheme(){
+        if (document.body.style.backgroundColor==='white') {
+            document.getElementById('btn').style.justifyContent="flex-end";
+            document.body.style.backgroundColor="black";
+            document.getElementById('aboutme').style.backgroundImage="url('images/blackbg.png')"
+            document.body.style.color='white';
+            document.getElementById('contactme').style.borderRight="white solid 2px";
+            
+        } else {
+            document.getElementById('btn').style.justifyContent="flex-start";
+            document.body.style.backgroundColor="white";
+            document.getElementById('aboutme').style.backgroundImage="url('images/whitebg.png')"
+            document.body.style.color='black';
+            document.getElementById('contactme').style.borderRight="black solid 2px";
+        }
     }
+
+    
