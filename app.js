d313192679ef2a98920f219864449f049d23999b
titiksha <titiksha.iyer@gmail.com>
2023-12-06 14:05:30 +0530

    added js file i guess

---
+++ app.js
@@ -0,0 +1,14 @@
+// let bodycolor=document.body.style.backgroundColor;
+// function changetheme(){
+//     bodycolor="black";
+    
+// }
+    function changetheme(){
+        if(document.body.style.backgroundColor==="black"){
+            document.body.style.backgroundColor="white";
+        }
+        else{
+            document.body.style.backgroundColor="black";
+        }
+        
+    }
